25488c14a86ca4fe577b3c0bddcd65ae3f646d77
周茂 <zhoumao@meituan.com>
2015-10-30 21:46:52 +0800

    scrollListView

---
+++ app/src/main/java/com/example/uilistviewtest/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.uilistviewtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+	private List<Fruit> fruitList = new ArrayList<Fruit>();
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		initFruits();
+		FruitAdapter adapter = new FruitAdapter(MainActivity.this,
+				R.layout.fruit_item, fruitList);
+		ListView listView = (ListView) findViewById(R.id.list_view);
+		ListView listView1 = (ListView)findViewById(R.id.list_view1);
+		listView1.setAdapter(adapter);
+		listView.setAdapter(adapter);
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+									int position, long id) {
+				Fruit fruit = fruitList.get(position);
+				Toast.makeText(MainActivity.this, fruit.getName(),
+						Toast.LENGTH_SHORT).show();
+			}
+		});
+		setListViewHeightBasedOnChildren(listView);
+		setListViewHeightBasedOnChildren(listView1);
+	}
+
+	private void initFruits() {
+		Fruit apple = new Fruit("Apple", R.drawable.apple_pic);
+		fruitList.add(apple);
+		Fruit banana = new Fruit("Banana", R.drawable.banana_pic);
+		fruitList.add(banana);
+		Fruit orange = new Fruit("Orange", R.drawable.orange_pic);
+		fruitList.add(orange);
+		Fruit watermelon = new Fruit("Watermelon", R.drawable.watermelon_pic);
+		fruitList.add(watermelon);
+		Fruit pear = new Fruit("Pear", R.drawable.pear_pic);
+		fruitList.add(pear);
+		Fruit grape = new Fruit("Grape", R.drawable.grape_pic);
+		fruitList.add(grape);
+		Fruit pineapple = new Fruit("Pineapple", R.drawable.pineapple_pic);
+		fruitList.add(pineapple);
+		Fruit strawberry = new Fruit("Strawberry", R.drawable.strawberry_pic);
+		fruitList.add(strawberry);
+		Fruit cherry = new Fruit("Cherry", R.drawable.cherry_pic);
+		fruitList.add(cherry);
+		Fruit mango = new Fruit("Mango", R.drawable.mango_pic);
+		fruitList.add(mango);
+	}
+
+	/**
+	 * 动态设置ListView的高度
+	 * @param listView
+	 */
+	public static void setListViewHeightBasedOnChildren(ListView listView) {
+		if(listView == null) return;
+		ListAdapter listAdapter = listView.getAdapter();
+		if (listAdapter == null) {
+			// pre-condition
+			return;
+		}
+		int totalHeight = 0;
+		for (int i = 0; i < listAdapter.getCount(); i++) {
+			View listItem = listAdapter.getView(i, null, listView);
+			listItem.measure(0, 0);
+			totalHeight += listItem.getMeasuredHeight();
+		}
+		ViewGroup.LayoutParams params = listView.getLayoutParams();
+		params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+		listView.setLayoutParams(params);
+	}
+
+}
